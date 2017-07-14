1a822968c04b36c6b559a012d5b63c6fa3acc54d
Jessica Tracy <j.tracy916@gmail.com>
2017-07-13 21:16:41 -0400

    gorilla/mux router

--- gapi.go
+++ gapi.go
@@ -1,17 +1,23 @@
 package main
 
+// use go get first
+// Also had to export GOBIN=$GOPATH/bin
 import (
     "fmt"
     "html"
     "log"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Index)
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
 
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
