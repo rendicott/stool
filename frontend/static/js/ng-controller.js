2d6a4c6be1d58191304645043690e2030fe08f02
Jessica Tracy <jessica.tracy@ge.com>
2017-08-18 22:09:11 -0400

    create functions

--- frontend/static/js/ng-controller.js
+++ frontend/static/js/ng-controller.js
@@ -23,7 +23,8 @@ angular.module('gapi', [])
                     },
                     function errorCallback(response) {
                         console.log("Unable to get game data...");
-                    });
+                    }
+                );
         };
 
         $scope.getPlayers = function() {
@@ -38,7 +39,8 @@ angular.module('gapi', [])
                     },
                     function errorCallback(response) {
                         console.log("Unable to get player data...");
-                    });
+                    }
+                );
         };
 
         $scope.getOutcomes = function() {
@@ -53,7 +55,68 @@ angular.module('gapi', [])
                     },
                     function errorCallback(response) {
                         console.log("Unable to get outcome data...");
-                    });
+                    }
+                );
+        };
+
+        $scope.createGame = function(name) {
+            console.log("In createGame function");
+
+            $http.post("/games", {"name": name})
+                .then(
+                    function successCallback(response) {
+                        console.log(response.data);
+                        console.log("adding game data to scope");
+                        $scope.allGames.push(response.data);
+                    },
+                    function errorCallback(response) {
+                        console.log("Unable to create game: ");
+                        console.log(response);
+                    }
+                );
+
+        };
+
+        $scope.createPlayer = function(name) {
+            console.log("In createPlayer function");
+
+            $http.post("/players", {"name": name})
+                .then(
+                    function successCallback(response) {
+                        console.log(response.data);
+                        console.log("adding player data to scope");
+                        $scope.allPlayers.push(response.data);
+                    },
+                    function errorCallback(response) {
+                        console.log("Unable to create player: ");
+                        console.log(response);
+                    }
+                );
+
+        };
+
+        $scope.createOutcome = function(gameId, playerId, win) {
+            console.log("In createGame function");
+
+            if (win === "true") {
+                var winBool = true;
+            } else {
+                var winBool = false;
+            }
+
+            $http.post("/outcomes", {"gameid": gameId, "playerid": playerId, "win": winBool})
+                .then(
+                    function successCallback(response) {
+                        console.log(response.data);
+                        console.log("adding outcome data to scope");
+                        $scope.allOutcomes.push(response.data);
+                    },
+                    function errorCallback(response) {
+                        console.log("Unable to create outcome: ");
+                        console.log(response);
+                    }
+                );
+
         };
 
 
